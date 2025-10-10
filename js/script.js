b6d24eddf0cf36e79480536838ad271f275995ed
PF <studiopeinturefigurine@gmail.com>
2025-10-10 12:05:25 +0200

    Update script.js
    Signed-off-by: PF <studiopeinturefigurine@gmail.com>

--- js/script.js
+++ js/script.js
@@ -1,4 +1,24 @@
+  let currentSlide = 0;
+        const slides = document.querySelectorAll('.carousel img');
+        const totalSlides = slides.length;
 
+        function moveSlide(direction) {
+            currentSlide += direction;
+
+            if (currentSlide >= totalSlides) {
+                currentSlide = 0;
+            } else if (currentSlide < 0) {
+                currentSlide = totalSlides - 1;
+            }
+
+            const carousel = document.getElementById('carousel');
+            carousel.style.transform = `translateX(-${currentSlide * 100}%)`;
+        }
+
+        // Auto-advance every 5 seconds
+        setInterval(() => {
+            moveSlide(1);
+        }, 5000);
 
           
     
