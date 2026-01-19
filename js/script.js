971d08a83c60cab1bb953d9c5d72224328015ce9
PF <studiopeinturefigurine@gmail.com>
2026-01-19 21:35:23 +0100

    Update script.js
    Signed-off-by: PF <studiopeinturefigurine@gmail.com>

--- js/script.js
+++ js/script.js
@@ -54,7 +54,8 @@ function changelanguefrench() {
       changelanguemenu();
    
 }
-
+function changelanguegalerie() {}
+function changelanguesimulateur() {}
 
 function changelanguemenu() {
     const menucontenu = document.getElementById("menu-contenu");
