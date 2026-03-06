49e86f37ae91e424930941ae0bd618627683290d
PF <studiopeinturefigurine@gmail.com>
2026-03-06 23:49:00 +0100

    Update script.js
    Signed-off-by: PF <studiopeinturefigurine@gmail.com>

--- js/script.js
+++ js/script.js
@@ -274,9 +274,7 @@ function changelanguemenu() {
     if (langueselect === "english") {
 
                menucontenu.innerHTML = `<ul class="menu">
-    <li><a href="peinturecommission.html" onclick="loadPage('peinturecommission.html'); changelangueindex(); return false;">Advanced TableTop Painting 🎨</a></li>
-    <li><a href="figconcours.html" onclick="loadPage('figconcours.html'); changelanguefigconcours(); return false;">Competition/Exhibition Miniatures 🏆</a></li>
-    <li><a href="piecepopculture.html" onclick="loadPage('piecepopculture.html');  changelanguepiecepop(); return false;">Geek/Pop Culture Figurines 🔥</a></li>
+    <li><a href="peinturecommission.html" onclick="loadPage('peinturecommission.html'); changelangueindex(); return false;">TableTop Painting 🎨</a></li>
     <li><a href="simulateur_devis.html" onclick="loadPage('simulateur_devis.html'); changelanguesimulateur(); return false;">Quote Simulator 💰</a></li>
     <li><a href="galerie.html" onclick="loadPage('galerie.html'); changelanguegalerie(); return false;">Gallery 🖼️</a></li>
 </ul>
@@ -285,9 +283,7 @@ function changelanguemenu() {
         if (langueselect === "spanish") {
 
                menucontenu.innerHTML = `<ul class="menu">
-    <li><a href="peinturecommission.html" onclick="loadPage('peinturecommission.html'); changelangueindex(); return false;">Pintura TableTop Avanzada 🎨</a></li>
-    <li><a href="figconcours.html" onclick="loadPage('figconcours.html'); changelanguefigconcours(); return false;">Miniaturas de Concurso/Exposición 🏆</a></li>
-    <li><a href="piecepopculture.html" onclick="loadPage('piecepopculture.html');  changelanguepiecepop(); return false;">Figurines Geek/Pop Culture 🔥</a></li>
+    <li><a href="peinturecommission.html" onclick="loadPage('peinturecommission.html'); changelangueindex(); return false;">Pintura TableTop 🎨</a></li>
     <li><a href="simulateur_devis.html" onclick="loadPage('simulateur_devis.html'); changelanguesimulateur(); return false;">Simulador de Presupuesto 💰</a></li>
     <li><a href="galerie.html" onclick="loadPage('galerie.html'); changelanguegalerie(); return false;">Galería 🖼️</a></li>
 </ul>
@@ -296,11 +292,9 @@ function changelanguemenu() {
         if (langueselect === "french") {
 
                menucontenu.innerHTML = `  <ul class="menu">
-                <li><a href="peinturecommission.html" onclick="loadPage('peinturecommission.html'); changelangueindex(); return false;">Peinture TableTop Supérieur 🎨</a></li>
-                <li><a href="figconcours.html" onclick="loadPage('figconcours.html'); changelanguefigconcours(); return false;">Figurines de concours/exposition 🏆</a></li>
-                <li><a href="piecepopculture.html" onclick="loadPage('piecepopculture.html');  changelanguepiecepop(); return false;">Figurine Geek/Pop Culture 🔥</a></li>
+                <li><a href="peinturecommission.html" onclick="loadPage('peinturecommission.html'); changelangueindex(); return false;">Commission Peinture 🎨</a></li>
                 <li><a href="formation.html" onclick="loadPage('formation.html'); return false;">Formation 📚</a></li>
-                <li><a href="simulateur_devis.html" onclick="loadPage('simulateur_devis.html'); changelanguesimulateur(); return false;">Simulateur de devis 💰</a></li>
+                <li><a href=" _devis.html" onclick="loadPage(' _devis.html'); return false;">Demande de devis 💰</a></li>
                 <li><a href="galerie.html" onclick="loadPage('galerie.html'); changelanguegalerie(); return false;">Galerie 🖼️</a></li>
             </ul>
 `
